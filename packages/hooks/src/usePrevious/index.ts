e10259ffa4f7d4d2439017985b4cb6856a6aa037
曹谊的home <cao1032398875@sina.com>
2025-09-17 16:01:18 +0800

    feat: usePrevious

--- packages/hooks/src/usePrevious/index.ts
+++ packages/hooks/src/usePrevious/index.ts
@@ -2,21 +2,35 @@ import { useRef } from 'react';
 
 export type ShouldUpdateFunc<T> = (prev: T | undefined, next: T) => boolean;
 
-const defaultShouldUpdate = <T>(a?: T, b?: T) => !Object.is(a, b);
+// const defaultShouldUpdate = <T>(a?: T, b?: T) => !Object.is(a, b);
 
-function usePrevious<T>(
-  state: T,
-  shouldUpdate: ShouldUpdateFunc<T> = defaultShouldUpdate,
-): T | undefined {
-  const prevRef = useRef<T>();
-  const curRef = useRef<T>();
+// function usePrevious<T>(
+//   state: T,
+//   shouldUpdate: ShouldUpdateFunc<T> = defaultShouldUpdate,
+// ): T | undefined {
+//   const prevRef = useRef<T>();
+//   const curRef = useRef<T>();
 
-  if (shouldUpdate(curRef.current, state)) {
-    prevRef.current = curRef.current;
-    curRef.current = state;
+//   if (shouldUpdate(curRef.current, state)) {
+//     prevRef.current = curRef.current;
+//     curRef.current = state;
+//   }
+
+//   return prevRef.current;
+// }
+
+const defaultUpdateFn = <T>(a?: T, b?: T) => !Object.is(a, b);
+
+function usePrevious<T>(state: T, shouldUpdateFn : ShouldUpdateFunc<T> = defaultUpdateFn): T | undefined{
+  const prev = useRef<T>()
+  const cur = useRef<T>()
+
+  if(shouldUpdateFn(state, cur.current)){
+    prev.current = cur.current
+    cur.current = state
   }
 
-  return prevRef.current;
+  return prev.current
 }
 
 export default usePrevious;
