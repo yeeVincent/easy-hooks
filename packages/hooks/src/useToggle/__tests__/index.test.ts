d0b6103e29061af54879e49041744d307381f524
曹谊的home <cao1032398875@sina.com>
2025-09-17 15:53:38 +0800

    feat: useToggle and test

--- packages/hooks/src/useToggle/__tests__/index.test.ts
+++ packages/hooks/src/useToggle/__tests__/index.test.ts
@@ -7,36 +7,71 @@ const callToggle = (hook: any) => {
   });
 };
 
-describe('useToggle', () => {
-  it('test on init', async () => {
-    const hook = renderHook(() => useToggle());
-    expect(hook.result.current[0]).toBeFalsy();
-  });
+// describe('useToggle', () => {
+//   it('test on init', async () => {
+//     const hook = renderHook(() => useToggle());
+//     expect(hook.result.current[0]).toBeFalsy();
+//   });
 
-  it('test on methods', async () => {
-    const hook = renderHook(() => useToggle('Hello'));
-    expect(hook.result.current[0]).toBe('Hello');
-    callToggle(hook);
-    expect(hook.result.current[0]).toBeFalsy();
-    act(() => {
-      hook.result.current[1].setLeft();
-    });
-    expect(hook.result.current[0]).toBe('Hello');
-    act(() => {
-      hook.result.current[1].setRight();
-    });
-    expect(hook.result.current[0]).toBeFalsy();
-  });
+//   it('test on methods', async () => {
+//     const hook = renderHook(() => useToggle('Hello'));
+//     expect(hook.result.current[0]).toBe('Hello');
+//     callToggle(hook);
+//     expect(hook.result.current[0]).toBeFalsy();
+//     act(() => {
+//       hook.result.current[1].setLeft();
+//     });
+//     expect(hook.result.current[0]).toBe('Hello');
+//     act(() => {
+//       hook.result.current[1].setRight();
+//     });
+//     expect(hook.result.current[0]).toBeFalsy();
+//   });
+
+//   it('test on optional', () => {
+//     const hook = renderHook(() => useToggle('Hello', 'World'));
+//     callToggle(hook);
+//     expect(hook.result.current[0]).toBe('World');
+//     act(() => {
+//       hook.result.current[1].set('World');
+//     });
+//     expect(hook.result.current[0]).toBe('World');
+//     callToggle(hook);
+//     expect(hook.result.current[0]).toBe('Hello');
+//   });
+// });
+
+describe('useToggle', () => {
+  it('test on init', () => {
+    const hook = renderHook(() => useToggle())
+    console.log("🚀 ~ it ~ hook:", hook)
+    expect(hook.result.current[0]).toBeFalsy()
+  })
+
+  it('test on methods', () => {
+    
+    const hook = renderHook(() => useToggle('Hello'))
+    expect(hook.result.current[0]).toBe('Hello')
+
+    callToggle(hook)
+    expect(hook.result.current[0]).toBeFalsy()
+
+    act(() => hook.result.current[1].setLeft())
+    expect(hook.result.current[0]).toBe('Hello')
+
+    act(() => hook.result.current[1].setRight())
+    expect(hook.result.current[0]).toBeFalsy()
+  })
 
   it('test on optional', () => {
-    const hook = renderHook(() => useToggle('Hello', 'World'));
-    callToggle(hook);
-    expect(hook.result.current[0]).toBe('World');
-    act(() => {
-      hook.result.current[1].set('World');
-    });
-    expect(hook.result.current[0]).toBe('World');
-    callToggle(hook);
-    expect(hook.result.current[0]).toBe('Hello');
-  });
-});
+    const hook = renderHook(() => useToggle('Hello', 'World'))
+    callToggle(hook)
+    expect(hook.result.current[0]).toBe('World')
+
+    act(() => hook.result.current[1].set('World'))
+    expect(hook.result.current[0]).toBe('World')
+
+    callToggle(hook)
+    expect(hook.result.current[0]).toBe('Hello')
+  })
+})
--- packages/hooks/src/useToggle/index.ts
+++ packages/hooks/src/useToggle/index.ts
@@ -7,32 +7,54 @@ export interface Actions<T> {
   toggle: () => void;
 }
 
-function useToggle<T = boolean>(): [boolean, Actions<T>];
+// function useToggle<T = boolean>(): [boolean, Actions<T>];
 
-function useToggle<T>(defaultValue: T): [T, Actions<T>];
+// function useToggle<T>(defaultValue: T): [T, Actions<T>];
 
-function useToggle<T, U>(defaultValue: T, reverseValue: U): [T | U, Actions<T | U>];
+// function useToggle<T, U>(defaultValue: T, reverseValue: U): [T | U, Actions<T | U>];
+
+function useToggle<T = boolean>(): [boolean, Actions<T>]; 
+function useToggle<T>(defaultValue : T): [T, Actions<T>]; 
+function useToggle<T, R>(defaultValue : T, reverseValue : R): [T | R, Actions<T | R>]; 
 
 function useToggle<D, R>(defaultValue: D = (false as unknown) as D, reverseValue?: R) {
-  const [state, setState] = useState<D | R>(defaultValue);
+  // const [state, setState] = useState<D | R>(defaultValue);
 
-  const actions = useMemo(() => {
-    const reverseValueOrigin = (reverseValue === undefined ? !defaultValue : reverseValue) as D | R;
+  // const actions = useMemo(() => {
+  //   const reverseValueOrigin = (reverseValue === undefined ? !defaultValue : reverseValue) as D | R;
 
-    const toggle = () => setState((s) => (s === defaultValue ? reverseValueOrigin : defaultValue));
-    const set = (value: D | R) => setState(value);
-    const setLeft = () => setState(defaultValue);
-    const setRight = () => setState(reverseValueOrigin);
+  //   const toggle = () => setState((s) => (s === defaultValue ? reverseValueOrigin : defaultValue));
+  //   const set = (value: D | R) => setState(value);
+  //   const setLeft = () => setState(defaultValue);
+  //   const setRight = () => setState(reverseValueOrigin);
 
+  //   return {
+  //     toggle,
+  //     set,
+  //     setLeft,
+  //     setRight,
+  //   };
+  // }, []);
+
+  // return [state, actions];
+
+  const [state, setState] = useState<D | R>(defaultValue)
+
+  const action = useMemo(() => {
+    const reverseValueOrigin = reverseValue !== undefined ? reverseValue : !defaultValue
+    const toggle = () => setState((s: D | R) => s === defaultValue ? reverseValueOrigin : defaultValue)
+    const set = (value: D | R) => setState(value)
+    const setLeft = () => setState(defaultValue)
+    const setRight = () => setState(reverseValueOrigin)
     return {
-      toggle,
-      set,
-      setLeft,
-      setRight,
-    };
-  }, []);
+        toggle,
+        set,
+        setLeft,
+        setRight
+    }
+  }, []) 
 
-  return [state, actions];
+  return [state, action]
 }
 
 export default useToggle;
