bf9262acca4a8307abb23eb3bb1fb6f8f2950685
曹谊的home <cao1032398875@sina.com>
2025-09-18 16:53:05 +0800

    feat: 添加注释

--- packages/hooks/src/useMount/index.ts
+++ packages/hooks/src/useMount/index.ts
@@ -2,6 +2,20 @@ import { useEffect } from 'react';
 import { isFunction } from '../utils';
 import isDev from '../utils/isDev';
 
+
+// let _deps
+
+// const useEffect = function(cb, deps){
+//   let isNoDeps = !deps
+
+//   let isDepsChange = _deps ? deps.every((el, i) => el === _deps[i]) : true
+
+//   if(isNoDeps || isDepsChange ){
+//     cb?.()
+//     _deps = deps
+//   }
+// }
+
 const useMount = (fn: () => void) => {
   if (isDev) {
     if (!isFunction(fn)) {
